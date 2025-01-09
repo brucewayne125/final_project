f17485010b08f21d0fb46d5b3b35a2eeaca101c0
brucewayne125 <snowdbat@gmail.com>
2025-01-09 16:15:47 +0300

    Home page adjustment

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="container">
-      <h1 className="text-center mt-4">Welcome to MyApp</h1>
+      <h1 className="text-center mt-4">Welcome to NoDelex</h1>
       <p className="text-center">Browse our products and enjoy shopping!</p>
     </div>
   );
